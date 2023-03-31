5ed853f6a8f5fd463db63563149340bb9e0225ba
clock509 <8kwchoi8@naver.com>
2023-04-01 01:38:04 +0900

    Unit test

--- README.md
+++ README.md
@@ -76,6 +76,26 @@ $ nest generate module
 ? What name would you like to use for the module? movies   ## Type module name after question as you want.
 ```
 
+## Unit test
+__Unit test는 모든 함수나 기능을 각각 따로 테스트하는 테스트를 의미한다. 서비스에서 분리된 유닛을 테스트하는 것이다.__
+- NestJS는 JS 테스팅 프레임워크인 Jest를 기본적으로 설치한다.
+- 또한 NestJS는 테스팅 관련 파일인 `~.spec.ts`로 끝나는 파일들을 자동으로 생성한다.
+- Jest는 이 `spec.ts` 파일을 찾아서 테스트를 실행한다.
+- `package.json`의 Jest 관련 명령어를 참조하여 유닛 테스트를 진행할 수 있다.
+```
+$ npm run test:cov   # test:watch
+? What name would you like to use for the module? movies   ## Type module name after question as you want.
+```
+
+## E2E test
+__E2E(End to end) test는 모든 시스템을 통합적으로 테스트하는 테스트를 의미한다.__
+- 예를 들어, '이 페이지로 가면 특정 페이지가 나와야 한다'거나, '특정 링크를 누르면 이 페이지로 이동해야 한다'는 등의 사용자 스토리에 기반해 테스트하는 것은 E2E 테스트이다.
+- `package.json`의 Jest 관련 명령어를 참조하여 유닛 테스트를 진행할 수 있다.
+```
+$ npm run test:e2e
+? What name would you like to use for the module? movies   ## Type module name after question as you want.
+```
+
 ## Support
 
 Nest is an MIT-licensed open source project. It can grow thanks to the sponsors and support by the amazing backers. If you'd like to join them, please [read more here](https://docs.nestjs.com/support).
--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import {NotFoundException} from "@nestjs/common";
 
 describe('MoviesService', () => {
   let service: MoviesService;
 
+  // 테스트를 하기 전 실행되는 beforeEach
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
@@ -12,7 +14,102 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+  /* 테스트 케이스를 작성하는 부분 */
+  // it: 테스트 케이스 시작
   it('should be defined', () => {
+    // expect(received): 테스트 행동
+    // expect().~~~(expected): received에 대한 테스트 결과로 나와야 하는 값은 expected임을 정의한다.
     expect(service).toBeDefined();
+  });
+
+  it('4', () => {
+    expect(2 + 3).toEqual(5);
+  });
+
+  describe('getAll()', () => {
+    it('should return an Array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array); // getAll()의 리턴값이 배열인지 확인
+    });
+  });
+
+  describe('getOne()', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'Test movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined(); // Movie 엔티티인지 확인
+      expect(movie.id).toEqual(1);
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual(`Not found with movie id(999)`);
+      }
+    });
+
+    describe('deleteOne()', () => {
+      it('deletes a movie', () => {
+        service.create({
+          title: 'Test movie',
+          genres: ['test'],
+          year: 2000,
+        });
+        const allMovies = service.getAll().length;
+        service.deleteOne(1);
+        const afterDelete = service.getAll().length;
+        expect(afterDelete).toEqual(allMovies - 1);
+      });
+
+      it('should return 404 error', () => {
+        try {
+          service.deleteOne(999);
+        } catch (e) {
+          expect(e).toBeInstanceOf(NotFoundException);
+          expect(e.message).toEqual(`Not found with movie id(999)`);
+        }
+      });
+    });
+
+    describe('create', () => {
+      it('should create a movie', () => {
+        const beforeCreate = service.getAll().length;
+        service.create({
+          title: 'Test movie',
+          genres: ['test'],
+          year: 2000,
+        });
+        const afterCreate = service.getAll().length;
+        expect(afterCreate).toBeGreaterThan(beforeCreate);
+      });
+    });
+
+    describe('update', () => {
+      it('should update a movie', () => {
+        service.create({
+          title: 'Test movie',
+          genres: ['test'],
+          year: 2000,
+        });
+        service.update(1, { title: 'Updated Test' });
+        const movie = service.getOne(1);
+        expect(movie.title).toEqual('Updated Test');
+      });
+
+      it('should throw a NotFoundException', () => {
+        try {
+          service.update(999, {});
+        } catch (e) {
+          expect(e).toBeInstanceOf(NotFoundException);
+          // expect(e.message).toEqual(`Not found with movie id(999)`);
+        }
+      });
+    });
   });
 });
